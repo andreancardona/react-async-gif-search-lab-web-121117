bced5b1957af6c645df1989e7fbb8e59e07deda3
github username <github email address>
2018-02-25 22:21:19 -0500

    Done.

---
+++ src/components/GifSearch.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+class GifSearch extends React.Component {
+  state = {
+    value: ""
+  };
+
+  getInputValue = event => {
+    //console.log("clicking", event.target);
+    this.setState({
+      value: event.target.value //get the value input
+    });
+  };
+
+  handleOnSubmit = event => {
+    event.preventDefault();
+    this.props.querySearch(this.state.value);
+    console.log(this.state.value);
+    this.setState({
+      value: ""
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <form onInput={this.getInputValue} onSubmit={this.handleOnSubmit}>
+          <label>
+            Enter a Search Term:
+            <input type="text" value={this.state.value} />
+          </label>
+          <br />
+
+          <input type="submit" value="submit" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default GifSearch;
